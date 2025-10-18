eae427326a0ee3c95d162a75a22f7a2c0c2447c6
Zzampar <zampar.luca@isisbassafriulana.edu.it>
2025-10-18 12:19:41 +0200

    primo commit

---
+++ 2025-10-17/es_8/converti_metri.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float  A;
+
+int main(){
+    printf("inserire lunghezza in metri");
+    scanf("%f", &A);
+    printf("in centimetri è %f," A/100);
+    printf("in millimetri è %f," A/1000);
+    printf("in chilometri è %f," A*1000);
+    
+    
+    return 0;
+}
