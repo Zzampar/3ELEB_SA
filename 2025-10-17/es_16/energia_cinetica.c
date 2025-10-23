eced9398e27de4e6b0a2558d7da02cea0f1e9353
Zzampar <zampar.luca@isisbassafriulana.edu.it>
2025-10-23 18:49:21 +0200

    Add files via upload

---
+++ 2025-10-17/es_16/energia_cinetica.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+    float massa;
+    float velocità;
+
+    printf("inserisci massa in kg");
+    scanf("%f", &massa);
+    printf("inserisci velocità in m/s");
+    scanf("%f", &velocita);
+    printf("l'energia cinetica è %f", 0.5*massa*1000/velocita*velocità);
+    // si possono usare nuemri decimali
+
+
+
+
+
+
+    return 0;
+}
