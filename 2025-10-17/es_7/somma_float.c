b2df94d4d37380ddd9cacfb4fd29c981e39bd1de
Zzampar <zampar.luca@isisbassafriulana.edu.it>
2025-10-23 17:35:25 +0200

    Update somma_float.c

--- 2025-10-17/es_7/somma_float.c
+++ 2025-10-17/es_7/somma_float.c
@@ -6,6 +6,17 @@ float L;
 
 int main(){
 
+    printf("inserisci primo numero");    
+    scanf("%f &P);
+    printf("inserisci secondo numero");
+    scanf("%f &L);
+    printf( "la somma dei due numeri è %f", L+P);
+    printf( "la differenza dei due numeri è %f", L-P);
+    printf( "il prodotto dei due numeri è %f", L*P);
+    printf( "la divisione dei due numeri è %f", L/P);
+    // in questo modo possono uscire numeri con la virgola
+    
     return 0;
+
 
 } 
