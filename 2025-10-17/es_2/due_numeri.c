d61be0e1b8b92f2bf808279c4b1e70670790c7d2
Zzampar <zampar.luca@isisbassafriulana.edu.it>
2025-10-23 17:19:25 +0200

    Update due_numeri.c

--- 2025-10-17/es_2/due_numeri.c
+++ 2025-10-17/es_2/due_numeri.c
@@ -5,7 +5,7 @@ int numero1 = 7;
 int numero2 = 5;
 
 int main(){
-printf("la somma di %d e %d è %d", numero1, numero2, numero1+numero2 );
+printf("la somma di %d  e %d è %d", numero1, numero2, numero1+numero2 );
     // mettendo %d dichiari l'inserimento di un variabile intera che poi specifichi 
     // dopo la virgola in ordine in base a come vengono scritte
     
@@ -13,4 +13,5 @@ printf("la somma di %d e %d è %d", numero1, numero2, numero1+numero2 );
 
 
 } 
+
 
