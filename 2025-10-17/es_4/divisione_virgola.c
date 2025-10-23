3978db79396a5c2cf3c22fcac0121e9cb6ad71f2
Zzampar <zampar.luca@isisbassafriulana.edu.it>
2025-10-23 17:22:15 +0200

    Update divisione_virgola.c

--- 2025-10-17/es_4/divisione_virgola.c
+++ 2025-10-17/es_4/divisione_virgola.c
@@ -4,7 +4,10 @@
 float numero1 = 33;
 float numero2 = 5;
 
+// la variabile float indica un numero decimale
+
 int main(){
     printf("divisione col resto = %f", numero1/numero2);
     return 0;
+
 } 
